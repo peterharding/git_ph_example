2a8771ebaab604ceadf1690984016fbe4215cedd
Peter Harding <peter.harding@freshgrade.com>
2018-03-20 11:30:58 -0700

    add brake interfaces

---
+++ bicycle/src/Brakes.ts
@@ -0,0 +1,3 @@
+export interface Brakes {
+  brakes: string;
+}
---
+++ bicycle/src/FrontSet.ts
@@ -0,0 +1,8 @@
+import { Brakes } from "./Brakes";
+
+export interface FrontSet {
+  brakes: Brakes;
+  fork: string;
+  grip: string;
+  shocks: string;
+}
