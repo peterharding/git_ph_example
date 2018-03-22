8b802c753a274efa3a5c17e28f237b71987d516c
Peter Harding <peter.harding@freshgrade.com>
2018-03-22 13:55:43 -0700

    add pedals

--- bicycle/src/MyBike.ts
+++ bicycle/src/MyBike.ts
@@ -5,6 +5,8 @@ import { Cogset } from "./Cogset";
 import { DiscBrakes } from "./DiscBrakes";
 import { Frame } from "./frame";
 import { NineSpeed } from "./NineSpeed";
+import { Pedal } from "./Pedal";
+import { PlatformPedal } from "./PlatformPedal";
 import { RoadWheel } from "./RoadWheel";
 import { Wheel } from "./wheel";
 
@@ -12,12 +14,14 @@ export class MyBike implements Bicycle {
   public brakes: Brakes;
   public frame: Frame;
   public gears: Cogset;
+  public pedal: Pedal;
   public wheel: Wheel;
 
   constructor() {
     this.brakes = new DiscBrakes();
     this.frame = new AluminumFrame();
     this.gears = new NineSpeed;
+    this.pedal = new PlatformPedal();
     this.wheel = new RoadWheel();
   }
 }
