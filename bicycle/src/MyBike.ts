23696383495efe30944dc75786d7017aca3a8aa1
Peter Harding <peter.harding@freshgrade.com>
2018-03-22 14:07:45 -0700

    add chain

--- bicycle/src/MyBike.ts
+++ bicycle/src/MyBike.ts
@@ -1,3 +1,5 @@
+import { Chain } from "./Chain";
+import { RustyChain } from "./RustyChain";
 import { Pedal } from "./Pedal";
 import { PlatformPedal } from "./PlatformPedal";
 import { DiscBrakes } from "./DiscBrakes";
@@ -10,12 +12,14 @@ import { Brakes } from "./Brakes";
 
 export class MyBike implements Bicycle {
   public brakes: Brakes;
+  public chain: Chain;
   public frame: Frame;
   public pedal: Pedal;
   public wheel: Wheel;
 
   constructor() {
     this.brakes = new DiscBrakes();
+    this.chain = new RustyChain();
     this.frame = new AluminumFrame();
     this.pedal = new PlatformPedal();
     this.wheel = new RoadWheel();
