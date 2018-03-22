63645bb8c6eaccbb3e84d83fda7dcc6d9d6bb579
Peter Harding <peter.harding@freshgrade.com>
2018-03-22 13:59:48 -0700

    add gears

--- bicycle/src/MyBike.ts
+++ bicycle/src/MyBike.ts
@@ -1,14 +1,18 @@
 import { AluminumFrame } from "./AluminumFrame";
 import { Bicycle } from "./Bicycle"
+import { Cogset } from "./Cogset";
 import { Frame } from "./frame";
+import { NineSpeed } from "./NineSpeed";
 import { RoadWheel } from "./RoadWheel";
 import { Wheel } from "./wheel";
 
 export class MyBike implements Bicycle {
   public frame: Frame;
+  public gears: Cogset;
   public wheel: Wheel;
 
   constructor() {
+    this.gears = new NineSpeed;
     this.frame = new AluminumFrame();
     this.wheel = new RoadWheel();
   }
