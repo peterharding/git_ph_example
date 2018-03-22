454627e9f4b8010a845a4c79d2da64b6ddeba516
Peter Harding <peter.harding@freshgrade.com>
2018-03-22 13:54:13 -0700

    add brakes

--- bicycle/src/MyBike.ts
+++ bicycle/src/MyBike.ts
@@ -1,19 +1,23 @@
 import { AluminumFrame } from "./AluminumFrame";
 import { Bicycle } from "./Bicycle"
+import { Brakes } from "./Brakes";
 import { Cogset } from "./Cogset";
+import { DiscBrakes } from "./DiscBrakes";
 import { Frame } from "./frame";
 import { NineSpeed } from "./NineSpeed";
 import { RoadWheel } from "./RoadWheel";
 import { Wheel } from "./wheel";
 
 export class MyBike implements Bicycle {
+  public brakes: Brakes;
   public frame: Frame;
   public gears: Cogset;
   public wheel: Wheel;
 
   constructor() {
-    this.gears = new NineSpeed;
+    this.brakes = new DiscBrakes();
     this.frame = new AluminumFrame();
+    this.gears = new NineSpeed;
     this.wheel = new RoadWheel();
   }
 }
